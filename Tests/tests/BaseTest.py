da03cf80b78ce3cb0cd9ab2409883e57f025614f
Sergii Dmytrenko <SEDO@simcorp.com>
2021-12-04 19:37:48 +0200

    Try ChromeDriverManager

--- Tests/tests/BaseTest.py
+++ Tests/tests/BaseTest.py
@@ -1,6 +1,6 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 from Constants import Constants
 from utils import API
@@ -11,13 +11,14 @@ class BaseTestCase(unittest.TestCase):
         API.Users.delete_all()
         API.Users.create(Constants.LOGIN)
 
+        # service = Service("./../chromedriver")
         options = webdriver.ChromeOptions()
         options.add_argument('ignore-certificate-errors')
 
         # service = Service("./../chromedriver.exe")
         # self.driver = webdriver.Chrome(service=service, profile=profile)
-        service = Service("./../chromedriver")
-        self.driver = webdriver.Chrome(service=service, chrome_options=options)
+        # self.driver = webdriver.Chrome(service=service, chrome_options=options)
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
     def tearDown(self) -> None:
         self.driver.quit()
