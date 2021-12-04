446055421826c4d09ae5d8ce8421751b079315c6
Sergii Dmytrenko <SEDO@simcorp.com>
2021-12-04 19:50:14 +0200

    Remove chromedriver binaries

--- Tests/tests/BaseTest.py
+++ Tests/tests/BaseTest.py
@@ -11,13 +11,8 @@ class BaseTestCase(unittest.TestCase):
         API.Users.delete_all()
         API.Users.create(Constants.LOGIN)
 
-        # service = Service("./../chromedriver")
         options = webdriver.ChromeOptions()
         options.add_argument('ignore-certificate-errors')
-
-        # service = Service("./../chromedriver.exe")
-        # self.driver = webdriver.Chrome(service=service, profile=profile)
-        # self.driver = webdriver.Chrome(service=service, chrome_options=options)
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
     def tearDown(self) -> None:
